1f88625d6a3d6d0eee4f53005d913f955aa773e5
BANCASEMPIONE\808 <bailo.marco.personale@gmail.com>
2025-04-02 11:32:01 +0200

    PTR7515 - Correzioni sulle description nella sotto sezione 9020  - Rilasciata come ver. 2.1.2.3

--- PSE.BusinessLogic/ManipulatorSection090.cs
+++ PSE.BusinessLogic/ManipulatorSection090.cs
@@ -63,8 +63,9 @@ namespace PSE.BusinessLogic
                                                     CapitalMarketValueReportingCurrency = AssignRequiredDecimal(posItem.Amount1Base_23),
                                                     TotalMarketValueReportingCurrency = AssignRequiredDecimal(posItem.Amount1Base_23),
                                                     Amount = AssignRequiredDecimal(posItem.Quantity_28),
-                                                    Description1 = AssignRequiredString(posItem.Description1_32),
-                                                    Description2 = BuildComposedDescription([AssignRequiredLong(posItem.NumSecurity_29).ToString(), AssignRequiredString(posItem.IsinIban_85)]),                                                      
+                                                    Description1 = AssignRequiredString(posItem.Description2_33),
+                                                    Description2 = AssignRequiredString(posItem.Description1_32),
+                                                    Description3 = BuildComposedDescription([AssignRequiredLong(posItem.NumSecurity_29).ToString(), AssignRequiredString(posItem.IsinIban_85)]),                                                      
                                                     PercentWeight = CalculatePercentWeight(totalAssets, posItem.Amount1Base_23),
                                                 };
                                                 summaryTo = new SummaryTo() {
